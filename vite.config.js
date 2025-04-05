97a099fd3f44bb37228afe0e6970d0d05fc7877d
Nileshku Kushwaha <119029577+nilesh8795@users.noreply.github.com>
2025-04-05 17:57:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // ✅ This line allows Render to detect the running port
+    port: 5173,       // Optional: you can specify or let Render auto-detect
+  },
 })
