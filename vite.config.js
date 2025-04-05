f2e290916698714f923ed3d03464f7f99880bf88
Nileshku Kushwaha <119029577+nilesh8795@users.noreply.github.com>
2025-04-05 18:05:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0', // ✅ This line allows Render to detect the running port
-    port: 5173,       // Optional: you can specify or let Render auto-detect
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['hr-dashboard-frontend-yoko.onrender.com'],
   },
 })
